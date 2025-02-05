f725a2fcd5c6be4ddcb85b44b7ab68cdef56bccf
mustafa <mustafaalikhan757@gmail.com>
2025-02-05 16:22:46 +0530

    Some change

--- models/post.js
+++ models/post.js
@@ -3,10 +3,6 @@ const suggestionSchema = require("./suggestion");
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-    },
     title: {
         type: String,
         required: true,
