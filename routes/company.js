372c2449f0cddad642c02df1a2216efedf493cd2
mustafa <mustafaalikhan757@gmail.com>
2025-02-10 23:21:05 +0530

    Some change

--- routes/company.js
+++ routes/company.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Notification = require("../models/notification");
 const { isCompanyEmployee } = require( "../middleware" );
 const Company = require( "../models/company" );
+const bcrypt = require("bcryptjs");
 
 router.get("/companyDashboard",isCompanyEmployee, async(req,res)=>{
     const {id} = req.params;
@@ -25,13 +26,16 @@ router.get("/companyEmployeeApproval/3fcdbc7c-a72c-474a-bbba-4b7d373f550f", asyn
 
 router.post("/companyEmployeeApproval/3fcdbc7c-a72c-474a-bbba-4b7d373f550f", async(req,res)=>{
     try{
+        const {password} = req.body.companyApproval;
         const companyEmployee = new Company(req.body.companyApproval);
+        const hashedPassword = await bcrypt.hash(password,10);
+        companyEmployee.password = hashedPassword;
         await companyEmployee.save();
+        console.log(companyEmployee);
         res.send("Application submit");
     }catch(err){
         console.log(err);
-    }
-    
+    }  
 });
 
 router.get("/companyLogin", (req,res)=>{
@@ -39,10 +43,17 @@ router.get("/companyLogin", (req,res)=>{
 });
 
 router.post("/companyLogin", async(req,res)=>{
-    const { email, password } = req.body;
+    const { employeeId, password } = req.body;
+    console.log(password);
     try{
-        const companyEmployee = await Company.findOne({email});
+        const companyEmployee = await Company.findOne({employeeId});
         console.log(companyEmployee);
+        const isMatch = await bcrypt.compare(companyEmployee.password, password);
+        if(!isMatch){
+            res.status(403).send("password is incorrect");
+        }
+        req.session.companyEmployeeId = companyEmployee._id;
+        res.redirect("/companyDashboard");
     }catch(err){
         console.log(err);
     }
