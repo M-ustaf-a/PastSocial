8a46dd100c84efe30e075f88bf8ee07fa56d6b1c
mustafa <mustafaalikhan757@gmail.com>
2025-06-02 22:51:02 +0530

    Some change

--- app.js
+++ app.js
@@ -59,6 +59,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+app.use('/static', express.static('public'));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
